bae9fb42512b0ca030776b5eb5a5eb84c2557953
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-16 10:03:06 +0200

    Export local storage repositories

---
+++ src/storage/index.ts
@@ -0,0 +1,6 @@
+// ===============================
+// src/storage/index.ts
+// ===============================
+
+export { albumRepository } from "./album-repository";
+export { memoryRepository } from "./memory-repository";
