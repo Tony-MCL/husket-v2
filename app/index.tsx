e47e22c20f870e5a6e56b53cc4060de4d8b4a8af
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-16 12:52:46 +0200

    Use library bookshelf as start screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
-import { AlbumListScreen } from "../src/features/albums/screens/AlbumListScreen";
+// ===============================
+// app/index.tsx
+// ===============================
+
+import { LibraryScreen } from "../src/features/library/screens/LibraryScreen";
 
 export default function IndexRoute() {
-  return <AlbumListScreen />;
+  return <LibraryScreen />;
 }
