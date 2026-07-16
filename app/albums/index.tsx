0b2f7bfeecc5ad9af4450db5ecdb11bf6d4a0921
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-16 12:52:40 +0200

    Add album management route

---
+++ app/albums/index.tsx
@@ -0,0 +1,9 @@
+// ===============================
+// app/albums/index.tsx
+// ===============================
+
+import { AlbumListScreen } from "../../src/features/albums/screens/AlbumListScreen";
+
+export default function AlbumsRoute() {
+  return <AlbumListScreen />;
+}
