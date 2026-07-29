0181491fd3b348b02fec4ba143e1800b0ccded4f
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-29 10:34:45 +0200

    Update LibraryScreen.tsx

--- src/features/library/screens/LibraryScreen.tsx
+++ src/features/library/screens/LibraryScreen.tsx
@@ -647,7 +647,7 @@ const styles = StyleSheet.create({
   cameraObject: {
     position: "absolute",
     left: "28.25%",
-    top: "16.5%",
+    top: "16.8%",
     width: "16%",
     height: "9.6%",
     zIndex: 18,
