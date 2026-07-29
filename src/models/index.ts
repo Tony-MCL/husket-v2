823d44b2fd460d3c88168733c5303bc565ebdf14
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-29 13:31:04 +0200

    Export multi-photo memory limit

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,6 +5,7 @@
 export type { Album } from "./album";
 export type { ImportedMemory } from "./imported-memory";
 export type { MemoryLocation } from "./location";
+export { MAX_MEMORY_MEDIA_ITEMS } from "./memory";
 export type { Memory, MemorySource } from "./memory";
 export type { MemoryMedia, MemoryMediaType } from "./media";
 export type { MemoryMood } from "./mood";
